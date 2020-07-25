a4a15b9c475809b5c8e5a18517b954179f4e605e
aaronamm <theeranitp@gmail.com>
2020-07-25 14:50:56 +0700

    Update Codesanook.Youtube with new version of dependent modules

--- Models/YouTubePart.cs
+++ Models/YouTubePart.cs
@@ -1,18 +1,8 @@
 ﻿using Orchard.ContentManagement;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 
-namespace Codesanook.Youtube.Models
-{
+namespace Codesanook.Youtube.Models {
     public class YouTubePart : ContentPart<YoutubePartRecord>
     {
-        public int Id
-        {
-            get { return this.Record.Id; }
-            set { this.Record.Id = value; }
-        }
         public string VideoId
         {
             get { return this.Record.VideoId; }
